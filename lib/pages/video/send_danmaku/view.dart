f0b53d0e8668c1960354ff427003ceb33430adc9
MU mu <w2744057454@gmail.com>
2026-08-29 00:17:29 +0800

    fix: chat panel compat shim + pin chat to dev d2f7b8d

--- lib/pages/video/send_danmaku/view.dart
+++ lib/pages/video/send_danmaku/view.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'package:PiliPlus/common/widgets/chat_panel_compat.dart';
 
 import 'package:PiliPlus/common/widgets/button/icon_button.dart';
 import 'package:PiliPlus/common/widgets/view_safe_area.dart';
